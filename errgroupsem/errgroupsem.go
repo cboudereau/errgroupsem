5ff0c29eb5aab7d6f0edf87e78c16517998a861e
Clement Boudereau <clement.boudereau@zen.ly>
2021-08-25 15:50:29 +0200

    init

---
+++ errgroupsem/errgroupsem.go
@@ -0,0 +1,57 @@
+package errgroupsem
+
+import (
+	"context"
+	"sync"
+
+	"golang.org/x/sync/semaphore"
+)
+
+type ErrGroupSem struct {
+	cancel func()
+
+	wg sync.WaitGroup
+
+	errOnce sync.Once
+	err     error
+	sem     *semaphore.Weighted
+}
+
+func WithContext(ctx context.Context, limit int) (*ErrGroupSem, context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	return &ErrGroupSem{cancel: cancel, sem: semaphore.NewWeighted(int64(limit))}, ctx
+}
+
+func (g *ErrGroupSem) Wait() error {
+	g.wg.Wait()
+	if g.cancel != nil {
+		g.cancel()
+	}
+	return g.err
+}
+
+func (g *ErrGroupSem) markFailed(err error) {
+	g.errOnce.Do(func() {
+		g.err = err
+		if g.cancel != nil {
+			g.cancel()
+		}
+	})
+}
+
+func (g *ErrGroupSem) Go(ctx context.Context, f func() error) {
+	if err := g.sem.Acquire(ctx, 1); err != nil {
+		g.markFailed(err)
+		return
+	}
+	g.wg.Add(1)
+
+	go func() {
+		defer g.sem.Release(1)
+		defer g.wg.Done()
+
+		if err := f(); err != nil {
+			g.markFailed(err)
+		}
+	}()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cboudereau/errgroupsem
+
+go 1.16
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
